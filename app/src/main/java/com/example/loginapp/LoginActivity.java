471743f3ca8d78e4ad129e3c3266d0c46cd0cbf1
Eric P <cire.ttalp@gmail.com>
2024-10-29 13:03:45 -0700

    Initial commit

---
+++ app/src/main/java/com/example/loginapp/LoginActivity.java
@@ -0,0 +1,37 @@
+package com.example.loginapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class LoginActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        final Button button = findViewById(R.id.button_id);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent nextBestThing = new Intent(LoginActivity.this, ActivityHome.class);
+                LoginActivity.this.startActivity(nextBestThing);
+            }
+        });
+
+    }
+}
